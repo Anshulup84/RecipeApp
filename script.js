18cc13adcfbd398295e0e9a548a0fbee4a77ff31
Anshul <avenger720720@gmail.com>
2025-03-22 15:27:11 +0530

    recipe card layout and functionality

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
-const searchBox = document.querySelector('.searchBox');
-const searchBtn = document.querySelector('.searchBtn');
-const recipeContainer = document.querySelector('.recipe-container');
+const searchBox = document.querySelector(".searchBox");
+const searchBtn = document.querySelector(".searchBtn");
+const recipeContainer = document.querySelector(".recipe-container");
 
 const fetchRecipes = async (query) => {
-    const data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`)
-    const response = await data.json()
+    recipeContainer.innerHTML = "<h2>Fetching Recipes...</h2>"
+  const data = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
+  );
+  const response = await data.json();
 
-    response.meals.forEach(meal => {
-        const recipeDiv = document.createElement('div')
-        recipeDiv.classList.add('recipe')
-        recipeDiv.innerHTML = `
+  recipeContainer.innerHTML = ""
+  response.meals.forEach((meal) => {
+    const recipeDiv = document.createElement("div");
+    recipeDiv.classList.add("recipe");
+    recipeDiv.innerHTML = `
             <img src="${meal.strMealThumb}">
+            <h3>${meal.strMeal}</h3>
+            <p><span>${meal.strArea}</span> Dish</p>
+             <p>Belongs to <span>${meal.strCategory}</span> Category</p>
             `
-            recipeContainer.appendChild(recipeDiv)
-            
-            
-    });
-}
+    const button = document.createElement('button') 
+    button.textContent = "View Recipe"       
+    recipeDiv.appendChild(button)
 
-searchBtn.addEventListener('click', (e) => {
- e.preventDefault()
-    const searchInput = searchBox.value.trim()
-    fetchRecipes(searchInput)
+    
+    recipeContainer.appendChild(recipeDiv);
+  });
+};
 
-})
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const searchInput = searchBox.value.trim();
+  fetchRecipes(searchInput);
+});
