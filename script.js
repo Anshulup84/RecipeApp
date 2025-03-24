3da5c485e65894217e94a3aa655ca32bfdbc8d0a
Anshul <avenger720720@gmail.com>
2025-03-24 15:55:47 +0530

    popup functionality and layout

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const searchBox = document.querySelector(".searchBox");
 const searchBtn = document.querySelector(".searchBtn");
 const recipeContainer = document.querySelector(".recipe-container");
+const recipeDetailsContent = document.querySelector(".recipe-details-content")
+const recipeCloseBtn = document.querySelector(".recipe-close-btn")
 
 const fetchRecipes = async (query) => {
     recipeContainer.innerHTML = "<h2>Fetching Recipes...</h2>"
@@ -23,10 +25,37 @@ const fetchRecipes = async (query) => {
     button.textContent = "View Recipe"       
     recipeDiv.appendChild(button)
 
-    
+    // Adding EventListener to recipe button
+    button.addEventListener('click', ()=> {
+      openRecipePopup(meal)
+    })
     recipeContainer.appendChild(recipeDiv);
   });
 };
+
+// Function to fetch ingredients and measurements
+const fetchIngredients = (meal) => {
+  let ingredientsList = "";
+  for(let i=1; i<=20; i++){
+    const ingredient = meal[`strIngredient${i}`]
+    if(ingredient){
+      const measure = meal[`strMeasure${i}`];
+      ingredientsList += `<li>${measure} ${ingredient}</li>`
+    }
+    else{
+      break;
+    }
+  }
+  return ingredientsList
+}
+const openRecipePopup = (meal) => {
+  recipeDetailsContent.innerHTML = `
+  <h2>${meal.strMeal}</h2>
+  <h3>Ingredients:</h3>
+  <ul>${fetchIngredients(meal)}</ul>
+  `
+  recipeDetailsContent.parentElement.style.display = 'block'
+}
 
 searchBtn.addEventListener("click", (e) => {
   e.preventDefault();
